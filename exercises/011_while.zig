585965f9b7ea9bb9303ee812895ed34451591f0c
Kristaps Baumanis <kristaps.baumanis.gmail.com>
2024-10-23 15:26:02 +0100

    solve exercises 001-044

--- exercises/011_while.zig
+++ exercises/011_while.zig
@@ -21,12 +21,9 @@ pub fn main() void {
     var n: u32 = 2;
 
     // Please use a condition that is true UNTIL "n" reaches 1024:
-    while (???) {
+    while (n < 1024) : (n *= 2) {
         // Print the current number
         std.debug.print("{} ", .{n});
-
-        // Set n to n multiplied by 2
-        n *= 2;
     }
 
     // Once the above is correct, this will print "n=1024"
